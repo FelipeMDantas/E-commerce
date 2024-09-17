dea6712e000211c80e030ac19c014103c4839e41
FelipeMDantas <felipemdantas@terra.com.br>
2024-09-16 22:23:22 -0300

    Assets added to the project.

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,7 @@
+export { default as About } from "./About";
+export { default as Cart } from "./Cart";
+export { default as Collection } from "./Collection";
+export { default as Contact } from "./Contact";
+export { default as Home } from "./Home";
+export { default as Login } from "./Login";
+export { default as Product } from "./Product";
