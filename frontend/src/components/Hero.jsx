e681bf98b67aa385a05299f52417cbd943271c58
FelipeMDantas <felipemdantas@terra.com.br>
2024-09-20 10:40:37 -0300

    Global state provider and new components developed.

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,3 +1,5 @@
+import { assets } from "../assets/assets";
+
 const Hero = () => {
   return (
     <div className="flex flex-col sm:flex-row border border-gray-400">
@@ -7,12 +9,16 @@ const Hero = () => {
             <p className="w-8 md:w-11 h-[2px] bg-[#414141]" />
             <p className="font-medium text-sm md:text-base">OUR BESTSELLERS</p>
           </div>
-          <h1 className="text-3xl sm:py-3 lg:text-5xl leading-relaxed">
+          <h1 className="prata-regular text-3xl sm:py-3 lg:text-5xl leading-relaxed">
             Latest Arrivals
           </h1>
-          <div className="flex items-center gap-2"></div>
+          <div className="flex items-center gap-2">
+            <p className="font-semibold text-sm md:text-base">SHOP NOW</p>
+            <p className="w-8 md:w-11 h-[1px] bg-[#414141]" />
+          </div>
         </div>
       </div>
+      <img src={assets.hero_img} alt="" className="w-full sm:w-1/2" />
     </div>
   );
 };
