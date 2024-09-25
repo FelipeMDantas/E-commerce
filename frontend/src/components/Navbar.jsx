a3a597612bb338ad59a6753c38f7b5a17df62da0
FelipeMDantas <felipemdantas@terra.com.br>
2024-09-24 22:44:11 -0300

    Searchbar component further developed.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,12 @@
 import { Link, NavLink } from "react-router-dom";
 import { assets } from "../assets/assets";
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
 
 const Navbar = () => {
   const [visible, setVisible] = useState(false);
+
+  const { setShowSearch } = useContext(ShopContext);
 
   return (
     <div className="flex items-center justify-between py-5 font-medium">
@@ -30,7 +33,12 @@ const Navbar = () => {
       </ul>
 
       <div className="flex items-center gap-6">
-        <img src={assets.search_icon} className="w-5 cursor-pointer" alt="" />
+        <img
+          src={assets.search_icon}
+          className="w-5 cursor-pointer"
+          alt=""
+          onClick={() => setShowSearch(true)}
+        />
 
         <div className="group relative">
           <img
--- frontend/src/components/Searchbar.jsx
+++ frontend/src/components/Searchbar.jsx
@@ -1,23 +1,46 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
+import { assets } from "../assets/assets";
+import { useLocation } from "react-router-dom";
 
 const Searchbar = () => {
   const { search, setSearch, showSearch, setShowSearch } =
     useContext(ShopContext);
 
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname.includes("collection") && showSearch) {
+      setShowSearch(true);
+    } else {
+      setShowSearch(false);
+    }
+  }, [location]);
+
   return (
-    <div className="border-t border-b bg-gray-50 text-center">
-      <div
-        className="inline-flex items-center justify-center border border-gray-400 px-5 py-2 my-5 mx-3 
+    showSearch && (
+      <div className="border-t border-b bg-gray-50 text-center">
+        <div
+          className="inline-flex items-center justify-center border border-gray-400 px-5 py-2 my-5 mx-3 
       rounded-full w-3/4 sm:w-1/2"
-      >
-        <input
-          type="text"
-          className="flex-1 outline-none bg-inherit text-sm"
-          placeholder="Search"
+        >
+          <input
+            type="text"
+            className="flex-1 outline-none bg-inherit text-sm"
+            placeholder="Search"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <img src={assets.search_icon} className="w-4" alt="" />
+        </div>
+        <img
+          src={assets.cross_icon}
+          className="inline w-3 cursor-pointer"
+          alt=""
+          onClick={() => setShowSearch(false)}
         />
       </div>
-    </div>
+    )
   );
 };
 
