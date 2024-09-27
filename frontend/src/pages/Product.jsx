eb1096b6e0eb1d8b70d2f1dc6d6351d744b69546
FelipeMDantas <felipemdantas@terra.com.br>
2024-09-26 23:44:59 -0300

    Products fetching structured.

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,4 +1,19 @@
+import { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { ShopContext } from "../context/ShopContext";
+
 const Product = () => {
+  const { productId } = useParams();
+  const { products } = useContext(ShopContext);
+
+  const [productData, setProductData] = useState(false);
+
+  const fetchProductData = async () => {};
+
+  useEffect(() => {
+    fetchProductData();
+  }, [productId, products]);
+
   return <div>Product</div>;
 };
 
