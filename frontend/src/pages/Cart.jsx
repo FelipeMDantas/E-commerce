1a5a7aa668655af7170207cbc8402b2988eeeba5
FelipeMDantas <felipemdantas@terra.com.br>
2024-10-04 21:44:00 -0300

    Cart items exhibition developed.

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -34,6 +34,42 @@ const Cart = () => {
           const productData = products.find(
             (product) => product._id === item._id
           );
+
+          return (
+            <div
+              key={index}
+              className="py-4 border-t border-b text-gray-700 grid grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols[4fr_2fr_0.5fr] 
+              items-center gap-4"
+            >
+              <div className="flex items-start gap-6">
+                <img
+                  src={productData.image[0]}
+                  className="w-16 sm:w-20"
+                  alt=""
+                />
+                <div>
+                  <p className="text-xs sm:text-lg font-medium">
+                    {productData.name}
+                  </p>
+                  <div className="flex items-center gap-5 mt-2">
+                    <p>
+                      {currency}
+                      {productData.price}
+                    </p>
+                    <p className="px-2 sm:px-3 sm:py-1 border bg-slate-50">
+                      {item.size}
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <input
+                type="number"
+                min={1}
+                defaultValue={item.quantity}
+                className="border max-w-10 sm:max-w-20 px-1 sm:px-2 py-1"
+              />
+            </div>
+          );
         })}
       </div>
     </div>
