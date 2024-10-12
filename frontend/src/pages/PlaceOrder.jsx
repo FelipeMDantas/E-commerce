5b8ce5115c5064ab1fa599c31a868981d64d3dd1
FelipeMDantas <felipemdantas@terra.com.br>
2024-10-11 22:22:50 -0300

    Different payment methods options styled.

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
+import { assets } from "../assets/assets";
 import CartTotal from "../components/CartTotal";
 import Title from "../components/Title";
 
 const PlaceOrder = () => {
+  const [method, setMethod] = useState("cod");
+
   return (
     <div className="flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t">
       <div className="flex flex-col gap-4 w-full sm:max-w-[480px]">
@@ -67,6 +71,31 @@ const PlaceOrder = () => {
         </div>
         <div className="mt-12">
           <Title text1="PAYMENT" text2="METHOD" />
+          <div className="flex gap-3 flex-col lg:flex-row">
+            <div
+              onClick={() => setMethod("stripe")}
+              className="flex items-center gap-3 border p-2 px-3 cursor-pointer"
+            >
+              <p className="min-w-3.5 h-3.5 border rounded-full" />
+              <img src={assets.stripe_logo} className="h-5 mx-4" alt="" />
+            </div>
+            <div
+              onClick={() => setMethod("razorpay")}
+              className="flex items-center gap-3 border p-2 px-3 cursor-pointer"
+            >
+              <p className="min-w-3.5 h-3.5 border rounded-full" />
+              <img src={assets.razorpay_logo} className="h-5 mx-4" alt="" />
+            </div>
+            <div
+              onClick={() => setMethod("cod")}
+              className="flex items-center gap-3 border p-2 px-3 cursor-pointer"
+            >
+              <p className="min-w-3.5 h-3.5 border rounded-full" />
+              <p className="text-gray-500 text-sm font-medium mx-4">
+                CASH ON DELIVERY
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
