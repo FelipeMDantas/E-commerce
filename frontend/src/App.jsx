272931ab1cbb0286f878aaea44db151b56de692f
FelipeMDantas <felipemdantas@terra.com.br>
2024-10-13 23:50:03 -0300

    Orders page structured.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import {
   Contact,
   Home,
   Login,
+  Orders,
   PlaceOrder,
   Product,
 } from "./pages";
@@ -30,6 +31,7 @@ const App = () => {
         <Route path="/cart" element={<Cart />} />
         <Route path="/login" element={<Login />} />
         <Route path="/place-order" element={<PlaceOrder />} />
+        <Route path="/orders" element={<Orders />} />
       </Routes>
       <Footer />
     </div>
