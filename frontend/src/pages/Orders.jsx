fadaa50be11b0dd4e5e961c1112f49cb3e5d3021
FelipeMDantas <felipemdantas@terra.com.br>
2024-10-14 23:30:55 -0300

    Orders page built.

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -30,7 +30,19 @@ const Orders = () => {
                   <p>Quantity: 1</p>
                   <p>Size: M</p>
                 </div>
+                <p className="mt-2">
+                  Date <span className="text-gray-400">25, Jul, 2024</span>
+                </p>
               </div>
+            </div>
+            <div className="md:w-1/2 flex justify-between">
+              <div className="flex items-center gap-2">
+                <p className="min-w-2 h-2 rounded-full bg-green-500" />
+                <p className="text-sm md:text-base">Ready to ship</p>
+              </div>
+              <button className="border px-4 py-2 text-sm font-medium rounded-sm">
+                Track Order
+              </button>
             </div>
           </div>
         ))}
