40100e6f5ea4f5d8720aa021dcec841aee3d73e7
FelipeMDantas <felipemdantas@terra.com.br>
2024-10-15 23:46:30 -0300

    Login page structured.

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,35 @@
+import { useState } from "react";
+
 const Login = () => {
-  return <div>Login</div>;
+  const [currentState, setCurrentState] = useState("Sign Up");
+  return (
+    <form className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800">
+      <div className="inline-flex items-center gap-2 mb-2 mt-10">
+        <p className="prata-regular text-3xl">{currentState}</p>
+        <hr className="border-none h-[1.5px] w-8 bg-gray-800" />
+      </div>
+      {currentState === "Login" && (
+        <input
+          type="text"
+          className="w-full px-3 py-2 border border-gray-800"
+          required
+          placeholder="Name"
+        />
+      )}
+      <input
+        type="email"
+        className="w-full px-3 py-2 border border-gray-800"
+        required
+        placeholder="Email"
+      />
+      <input
+        type="password"
+        className="w-full px-3 py-2 border border-gray-800"
+        required
+        placeholder="Password"
+      />
+    </form>
+  );
 };
 
 export default Login;
