7ea569dcb10d1b0eb5b8d6823e15ac2a1ec81622
FelipeMDantas <felipemdantas@terra.com.br>
2024-10-18 11:17:15 -0300

    About page further developed and Contact page structured.

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,5 +1,22 @@
+import { assets } from "../assets/assets";
+import Title from "../components/Title";
+
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div>
+      <div className="text-center text-2xl pt-10 border-t">
+        <Title text1="CONTACT" text2="US" />
+      </div>
+
+      <div className="my-10 flex flex-col justify-center md:flex-row gap-10 mb-28">
+        <img
+          src={assets.contact_img}
+          className="w-full md:max-w-[480px]"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
