a1935a32bf5d42cbb39c8f62cae23aaf65672101
FelipeMDantas <felipemdantas@terra.com.br>
2024-10-22 21:35:21 -0300

    Backend connection established.

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,15 @@
 import express from "express";
 import "dotenv/config";
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("API Working");
+});
+
+app.listen(port, () => console.log(`Server started on PORT: ${port}`));
