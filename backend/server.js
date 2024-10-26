80875f2dbdae0fa218be2bd00f98f9733430729c
FelipeMDantas <felipemdantas@terra.com.br>
2024-10-25 23:00:47 -0300

    Connection to Cloudinary configured.

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,12 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import connectToDB from "./config/mongodb.js";
+import connectToCloudinary from "./config/cloudinary.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
 connectToDB();
+connectToCloudinary();
 
 app.use(express.json());
 app.use(cors());
