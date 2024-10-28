442e2102a6b2f26c32357149afb65abb6534b2b4
FelipeMDantas <felipemdantas@terra.com.br>
2024-10-28 20:15:56 -0300

    User related controller and routes sketched.

---
+++ backend/controllers/userController.js
@@ -0,0 +1,7 @@
+const loginUser = async (req, res) => {};
+
+const registerUser = async (req, res) => {};
+
+const adminLogin = async (req, res) => {};
+
+export { loginUser, registerUser, adminLogin };
