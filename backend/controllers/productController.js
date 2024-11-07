44dab364c599ad1e7d9429428d574254117d946c
FelipeMDantas <felipemdantas@terra.com.br>
2024-11-06 23:43:05 -0300

    Product addition function further developed.

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,4 +1,24 @@
-const addProduct = async (req, res) => {};
+const addProduct = async (req, res) => {
+  try {
+    const {
+      name,
+      description,
+      price,
+      category,
+      subCategory,
+      sizes,
+      bestSeller,
+    } = req.body;
+
+    const image1 = req.files.image1[0];
+    const image2 = req.files.image2[0];
+    const image3 = req.files.image3[0];
+    const image4 = req.files.image4[0];
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 const listProducts = async (req, res) => {};
 
