baa71986ddc3e5229c3e39d8248c9c05919c8f8c
FelipeMDantas <felipemdantas@terra.com.br>
2024-11-12 23:36:44 -0300

    Single product data retrieval function built.

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -74,6 +74,16 @@ const removeProduct = async (req, res) => {
   }
 };
 
-const singleProduct = async (req, res) => {};
+const singleProduct = async (req, res) => {
+  try {
+    const { productId } = req.body;
+    const product = await productModel.findById(productId);
+
+    res.json({ success: true, product });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 export { listProducts, addProduct, removeProduct, singleProduct };
