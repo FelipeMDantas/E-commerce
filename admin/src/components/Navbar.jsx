7ca67452ac89e18add03e7e95f13d72f3c03b3f7
FelipeMDantas <felipemdantas@terra.com.br>
2024-11-23 23:42:07 -0300

    [feat] manage session token and logout

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,10 +1,13 @@
 import { assets } from "../assets/assets";
 
-const Navbar = () => {
+const Navbar = ({ setToken }) => {
   return (
     <div className="flex items-center py-2 px-[4%] justify-between">
       <img src={assets.logo} alt="" className="w-[max(10%,80px)]" />
-      <button className="bg-gray-600 text-white px-5 py-2 sm:px-9 sm:py-3 rounded-full text-xs sm:text-sm">
+      <button
+        className="bg-gray-600 text-white px-5 py-2 sm:px-9 sm:py-3 rounded-full text-xs sm:text-sm"
+        onClick={() => setToken("")}
+      >
         Logout
       </button>
     </div>
