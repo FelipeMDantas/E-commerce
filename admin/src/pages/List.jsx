57416ade875b33359f985901cdfb476a4be19634
FelipeMDantas <felipemdantas@terra.com.br>
2024-11-29 23:46:34 -0300

    [feat] Retrieve list of products

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -1,4 +1,21 @@
+import { useEffect, useState } from "react";
+import { backendUrl } from "../App";
+import axios from "axios";
+
 const List = () => {
+  const [list, setList] = useState([]);
+
+  const fetchList = async () => {
+    try {
+      const response = await axios.get(backendUrl + "/api/product/list");
+      console.log(response.data);
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    fetchList();
+  }, []);
+
   return <div>List</div>;
 };
 
