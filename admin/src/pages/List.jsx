b4f7c7bc84ef9b3460150a5c236873f6cacc285a
FelipeMDantas <felipemdantas@terra.com.br>
2024-11-30 22:45:10 -0300

    [feat] Structure products list table

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { backendUrl } from "../App";
 import axios from "axios";
+import { toast } from "react-toastify";
 
 const List = () => {
   const [list, setList] = useState([]);
@@ -8,15 +9,38 @@ const List = () => {
   const fetchList = async () => {
     try {
       const response = await axios.get(backendUrl + "/api/product/list");
-      console.log(response.data);
-    } catch (error) {}
+      if (response.data.products) {
+        setList(response.data.success);
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
   };
 
   useEffect(() => {
     fetchList();
   }, []);
 
-  return <div>List</div>;
+  return (
+    <>
+      <p className="mb-2">All Products List</p>
+      <div className="flex flex-col gap-2">
+        <div
+          className="hidden md:grid grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center py-1 px-2 border bg-gray-100
+          text-sm"
+        >
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b>Action</b>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default List;
