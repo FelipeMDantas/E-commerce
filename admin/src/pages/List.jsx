aedaa99855644cf2875c454f8eeac1b66c7a24ae
FelipeMDantas <felipemdantas@terra.com.br>
2024-12-01 22:55:43 -0300

    [feat] Style products list table

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { backendUrl } from "../App";
+import { backendUrl, currency } from "../App";
 import axios from "axios";
 import { toast } from "react-toastify";
 
@@ -9,8 +9,8 @@ const List = () => {
   const fetchList = async () => {
     try {
       const response = await axios.get(backendUrl + "/api/product/list");
-      if (response.data.products) {
-        setList(response.data.success);
+      if (response.data.success) {
+        setList(response.data.products);
       } else {
         toast.error(response.data.message);
       }
@@ -36,8 +36,26 @@ const List = () => {
           <b>Name</b>
           <b>Category</b>
           <b>Price</b>
-          <b>Action</b>
+          <b className="text-center">Action</b>
         </div>
+        {list.map((item, index) => (
+          <div
+            className="grid grid-cols-[1fr_3fr_1fr] md:grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center gap-2 py-1 px-2
+            border text-sm"
+            key={index}
+          >
+            <img className="w-12" src={item.image[0]} alt="" />
+            <p>{item.name}</p>
+            <p>{item.category}</p>
+            <p>
+              {currency}
+              {item.price}
+            </p>
+            <p className="text-right md:text-center cursor-pointer text-lg">
+              X
+            </p>
+          </div>
+        ))}
       </div>
     </>
   );
