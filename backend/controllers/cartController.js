316c08da4f1aa49d706e0d116d04b3df6f89610f
FelipeMDantas <felipemdantas@terra.com.br>
2024-12-13 23:46:32 -0300

    [feat] Code add to cart functionality

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -1,4 +1,26 @@
-const addToCart = async (req, res) => {};
+import userModel from "../models/userModel.js";
+
+const addToCart = async (req, res) => {
+  try {
+    const { userId, itemId, size } = req.body;
+
+    const userData = await userModel.findById(userId);
+    let cartData = await userData.cartData;
+
+    if (cartData[itemId]) {
+      if (cartData[itemId][size]) {
+        cartData[itemId][size] += 1;
+      } else {
+        cartData[itemId][size] = 1;
+      }
+    } else {
+      cartData[itemId] = {};
+      cartData[itemId][size] = 1;
+    }
+
+    await userModel.findByIdAndUpdate(userId, { cartData });
+  } catch (error) {}
+};
 const updateCart = async (req, res) => {};
 const getUserCart = async (req, res) => {};
 
