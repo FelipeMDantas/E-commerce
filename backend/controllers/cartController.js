8afc6a5164a23005055b13f83c9729954c392b3d
FelipeMDantas <felipemdantas@terra.com.br>
2024-12-16 23:36:14 -0300

    [feat] Add cart data to context provider

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -50,7 +50,7 @@ const getUserCart = async (req, res) => {
     const userData = await userModel.findById(userId);
     let cartData = await userData.cartData;
 
-    res.json({ success: false, message: error.message });
+    res.json({ success: true, cartData });
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: error.message });
