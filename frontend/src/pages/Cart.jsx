ab1530b719bfc746f35132c4b0da553bbda2fc53
FelipeMDantas <felipemdantas@terra.com.br>
2024-12-17 23:46:33 -0300

    [fix] Check amount of items during cart dynamic update

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -11,19 +11,21 @@ const Cart = () => {
   const [cartData, setCartData] = useState([]);
 
   useEffect(() => {
-    const tempData = [];
-    for (const items in cartItems) {
-      for (const item in cartItems[items]) {
-        if (cartItems[items][item] > 0) {
-          tempData.push({
-            _id: items,
-            size: item,
-            quantity: cartItems[items][item],
-          });
+    if (products.length > 0) {
+      const tempData = [];
+      for (const items in cartItems) {
+        for (const item in cartItems[items]) {
+          if (cartItems[items][item] > 0) {
+            tempData.push({
+              _id: items,
+              size: item,
+              quantity: cartItems[items][item],
+            });
+          }
         }
       }
+      setCartData(tempData);
     }
-    setCartData(tempData);
   }, [cartItems]);
 
   return (
