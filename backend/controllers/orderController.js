8e3b2848336ad5e695433a02710a80e8ea769613
FelipeMDantas <felipemdantas@terra.com.br>
2024-12-19 22:14:30 -0300

    [feat] Define order controller functions

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,20 @@
+const placeOrder = async (req, res) => {};
+
+const placeOrderStripe = async (req, res) => {};
+
+const placeOrderRazorPay = async (req, res) => {};
+
+const allOrders = async (req, res) => {};
+
+const userOrders = async (req, res) => {};
+
+const updateStatus = async (req, res) => {};
+
+export {
+  placeOrder,
+  placeOrderRazorPay,
+  placeOrderStripe,
+  allOrders,
+  updateStatus,
+  userOrders,
+};
