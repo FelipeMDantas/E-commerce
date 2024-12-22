eec96b415f6036975969ed36a1455458cf545533
FelipeMDantas <felipemdantas@terra.com.br>
2024-12-21 22:28:17 -0300

    [feat] placeOrder function further developed

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,4 +1,30 @@
-const placeOrder = async (req, res) => {};
+import orderModel from "../models/orderModel.js";
+import userModel from "../models/userModel.js";
+
+const placeOrder = async (req, res) => {
+  try {
+    const { userId, items, amount, address } = req.body;
+
+    const orderData = {
+      userId,
+      items,
+      address,
+      amount,
+      paymentMethod: "COD",
+      payment: false,
+      data: Date.now(),
+    };
+
+    const newOrder = new orderModel(orderData);
+    await newOrder.save();
+
+    await userModel.findByIdAndUpdate(userId, { cartData: {} });
+    res.json({ success: true, message: "Order Placed" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 const placeOrderStripe = async (req, res) => {};
 
