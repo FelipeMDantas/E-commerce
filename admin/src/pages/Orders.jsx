bb4568680b66d9cb0921944252667cadfd61dd1a
FelipeMDantas <felipemdantas@terra.com.br>
2025-01-01 13:11:12 -0300

    [feat] Further develop admin Orders page

--- admin/src/pages/Orders.jsx
+++ admin/src/pages/Orders.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { backendUrl } from "../App";
 import { toast } from "react-toastify";
+import { assets } from "../assets/assets";
 
 const Orders = ({ token }) => {
   const [orders, setOrders] = useState([]);
@@ -30,7 +31,35 @@ const Orders = ({ token }) => {
     fetchAllOrders();
   }, [token]);
 
-  return <div>Orders</div>;
+  return (
+    <div>
+      <h3>Order Page</h3>
+      <div>
+        {orders.map((order, index) => (
+          <div key={index}>
+            <img src={assets.parcel_icon} alt="" />
+            <div>
+              {order.items.map((item, index) => {
+                if (index === order.items.length - 1) {
+                  return (
+                    <p key={index}>
+                      {item.name} x {item.quantity} <span>{item.size}</span>
+                    </p>
+                  );
+                } else {
+                  return (
+                    <p key={index}>
+                      {item.name} x {item.quantity} <span>{item.size}</span>
+                    </p>
+                  );
+                }
+              })}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Orders;
