778079ff1fad910453f3ed77c3b2322b929f952d
FelipeMDantas <felipemdantas@terra.com.br>
2025-01-01 13:00:28 -0300

    [feat] Fetch orders data from UI

--- admin/src/pages/Orders.jsx
+++ admin/src/pages/Orders.jsx
@@ -1,4 +1,35 @@
-const Orders = () => {
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { backendUrl } from "../App";
+import { toast } from "react-toastify";
+
+const Orders = ({ token }) => {
+  const [orders, setOrders] = useState([]);
+
+  const fetchAllOrders = async () => {
+    if (!token) {
+      return null;
+    }
+
+    try {
+      const response = await axios.get(backendUrl + "/api/order/list", {
+        headers: { token },
+      });
+
+      if (response.data.success) {
+        setOrders(response.data.orders);
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchAllOrders();
+  }, [token]);
+
   return <div>Orders</div>;
 };
 
