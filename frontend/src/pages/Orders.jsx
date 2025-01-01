5dce0eea11ea05f5532e7a52fadc1910a10391b8
FelipeMDantas <felipemdantas@terra.com.br>
2024-12-31 22:22:39 -0300

    [feat] Code allOrders (retrieval) function

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -59,7 +59,7 @@ const Orders = () => {
               <img src={item.image[0]} className="w-16 sm:w-20" alt="" />
               <div>
                 <p className="sm:text-base font-medium">{item.name}</p>
-                <div className="flex items-center gap-3 mt-2 text-base text-gray-700">
+                <div className="flex items-center gap-3 mt-1 text-base text-gray-700">
                   <p className="text-lg">
                     {currency}
                     {item.price}
@@ -67,13 +67,13 @@ const Orders = () => {
                   <p>Quantity: {item.quantity}</p>
                   <p>Size: {item.size}</p>
                 </div>
-                <p className="mt-2">
+                <p className="mt-1">
                   Date:{" "}
                   <span className="text-gray-400">
                     {new Date(item.date).toDateString()}
                   </span>
                 </p>
-                <p className="mt-2">
+                <p className="mt-1">
                   Payment Method:{" "}
                   <span className="text-gray-400">{item.paymentMethod}</span>
                 </p>
@@ -82,9 +82,12 @@ const Orders = () => {
             <div className="md:w-1/2 flex justify-between">
               <div className="flex items-center gap-2">
                 <p className="min-w-2 h-2 rounded-full bg-green-500" />
-                <p className="text-sm md:text-base">Ready to ship</p>
+                <p className="text-sm md:text-base">{item.status}</p>
               </div>
-              <button className="border px-4 py-2 text-sm font-medium rounded-sm">
+              <button
+                onClick={loadOrderData}
+                className="border px-4 py-2 text-sm font-medium rounded-sm"
+              >
                 Track Order
               </button>
             </div>
